bf840db850a95b89ae76ad233388a711db119185
Edzz <edusuuu@user.noreply.github.com>
2024-01-02 16:31:02 +0800

    second commit.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import express from "express"
+let express = require("express")
+let cors = require("cors")
 
 let app = express()
 let PORT = 6969
 
-app.use(express.static('public'))
-
+app.use(cors())
+app.use(express.static(`${__dirname}/public`))
+ 
+app.get("/signin", (_, res) => {
+    res.sendFile(`${__dirname}/public/signin.html`);
+})
 
 let handleServerConnection = (error) => console.log(error ? `${error.message}` : `Server is running on port ${PORT}`)
 app.listen(PORT, handleServerConnection)
 // localhost:6969
 // 127.0.0.1:6969
+// demeco-dev
+// https://github.com/edusuuu/demeco-dev
+// 143.44.144.248
